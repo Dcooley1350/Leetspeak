67694f5832ff46f75cbb0abaf6ef8a6d8794eea6
andrew peterson and devin cooley <andrew@>
2019-09-30 11:57:23 -0700

    add more rules and ui

--- ModelTests/L1337Speak.Tests.cs
+++ ModelTests/L1337Speak.Tests.cs
@@ -24,5 +24,45 @@ namespace L1337Speak.Tests
             string correctString = "This is 3lit3.";
             Assert.AreEqual(correctString, newString);
         }
+        [TestMethod]
+        public void OtoZero_ChangesOtoNumberZero_true()
+        {
+            L1337speak newL1337speak = new L1337speak ("This is not Elite.");
+            char[] array = newL1337speak.StringToArray();
+            L1337speak.OToZero(array);
+            string newString = string.Join("", array);
+            string correctString = "This is n0t Elite.";
+            Assert.AreEqual(correctString, newString);
+        }
+        [TestMethod]
+        public void ItoOne_ChangesItoNumberOne_true()
+        {
+            L1337speak newL1337speak = new L1337speak ("I am Elite.");
+            char[] array = newL1337speak.StringToArray();
+            L1337speak.IToOne(array);
+            string newString = string.Join("", array);
+            string correctString = "1 am Elite.";
+            Assert.AreEqual(correctString, newString);
+        }
+        [TestMethod]
+        public void TtoSeven_ChangesTtoSeven_true()
+        {
+            L1337speak newL1337speak = new L1337speak ("I am Elite.");
+            char[] array = newL1337speak.StringToArray();
+            L1337speak.TtoSeven(array);
+            string newString = string.Join("", array);
+            string correctString = "I am Eli7e.";
+            Assert.AreEqual(correctString, newString);
+        }
+        [TestMethod]
+        public void StoZ_ChangesStoZ_true()
+        {
+            L1337speak newL1337speak = new L1337speak ("I am Elites.");
+            char[] array = newL1337speak.StringToArray();
+            L1337speak.StoZ(array);
+            string newString = string.Join("", array);
+            string correctString = "I am Elitez.";
+            Assert.AreEqual(correctString, newString);
+        }
     }
 }
